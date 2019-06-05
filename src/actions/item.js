884a3012ab0f5f23f88ace8737f8795d391c2298
Kevin <stradtkt22@gmail.com>
2019-06-05 08:23:00 -0400

    Added to the getItems action function

--- src/actions/item.js
+++ src/actions/item.js
@@ -1,9 +1,15 @@
 import {GET_ITEMS, ADD_ITEM, DELETE_ITEM, ITEMS_LOADING} from './../actions/types';
+import axios from 'axios';
 
-export const getItems = () => {
-    return {
-        type: GET_ITEMS
-    }
+export const getItems = () => dispatch => {
+    dispatch(setItemsLoading());
+    axios
+        .get('/api/items')
+        .then(res => dispatch({
+            type: GET_ITEMS,
+            payload: res.data
+        }))
+        .catch(err => console.error(err.message));
 }
 
 export const deleteItem = id => {
