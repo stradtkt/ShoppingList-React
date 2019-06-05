7e4a171cb3214a97a2dfcc37559b9c8d200c3fa4
Kevin <stradtkt22@gmail.com>
2019-06-05 05:45:04 -0400

    Added the start of the ShoppingList

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, {Fragment} from 'react';
-import AppNavbar from './components/main/AppNavbar';
-
+import AppNavbar from './components/layout/AppNavbar';
+import ShoppingList from './components/layout/ShoppingList';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
@@ -9,6 +9,7 @@ const App = () => {
   return (
     <Fragment>
         <AppNavbar/>
+        <ShoppingList/>
     </Fragment>
   );
 }
