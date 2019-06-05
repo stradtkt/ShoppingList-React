b4313838ff4bb9d19247d6405b0ebeaf9a47df9e
Kevin <stradtkt22@gmail.com>
2019-06-05 05:58:08 -0400

    Added a redux store file

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React, {Fragment} from 'react';
 import AppNavbar from './components/layout/AppNavbar';
 import ShoppingList from './components/layout/ShoppingList';
-
+import {Provider} from 'react-redux';
+import store from './store';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 const App = () => {
   return (
-    <Fragment>
-        <AppNavbar/>
-        <ShoppingList/>
-    </Fragment>
+    <Provider store={store}>
+        <Fragment>
+            <AppNavbar/>
+            <ShoppingList/>
+        </Fragment>
+    </Provider>
   );
 }
 
