3ea75c78db45bdf17a3ee255c69c247e8bb526fe
Kevin <stradtkt22@gmail.com>
2019-06-05 03:54:23 -0400

    Added the delete route

--- server/routes/api/items.js
+++ server/routes/api/items.js
@@ -17,4 +17,10 @@ router.post('/', (req, res) => {
         .then(item => res.json(item));
 });
 
+router.delete('/:id', (req, res) => {
+    Item.findById(req.params.id)
+        .then(item => item.remove().then(() => res.json({success: true})))
+        .catch(err => res.status(404).json({success: false}));
+});
+
 module.exports = router;
